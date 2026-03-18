d64881752eb1b98f4a79726d2173276d0bfd2810
jeman618 <jcisne23@calpoly.edu>
2026-03-17 21:49:12 -0700

    Changed some small things

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,5 +57,4 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation("androidx.datastore:datastore-preferences:1.2.0")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,5 +22,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Guru Calc"
+rootProject.name = "GuruCalc"
 include(":app")
